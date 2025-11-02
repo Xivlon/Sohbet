4fe84f06a90b3719195b5cb37f299337856f93ad
Nuruddin <104330069+Xivlon@users.noreply.github.com>
2025-11-02 17:03:22 +0300

    Set default API and WS URLs in config

--- frontend/app/lib/config.ts
+++ frontend/app/lib/config.ts
@@ -1,1 +1,2 @@
-export const API_URL: string | undefined = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL: string | undefined = process.env.NEXT_PUBLIC_API_URL || 'https://sohbet-uezxqq.fly.dev';
+export const WS_URL: string | undefined = process.env.NEXT_PUBLIC_WS_URL || 'wss://sohbet-uezxqq.fly.dev:8081';
